5fad038106da9ac87df2da4d4e77aed76ffbc19d
Mayankyadav3980 <ymayank501@gmail.com>
2025-03-27 21:30:15 +0530

    Business Details implemented

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,47 @@ const schema = {
   properties: {
     name: {
       // description: "Name of the Business",
-      title: "name",
+      title: "Business name",
       type: "string",
     },
     GSTIN: {
       type: "string",
-      pattern: "^[A-Z]{3}-\\d{3}$",
+      title: "GSTIN (ex: 29A123456789L1Z)",
+      minLength: 15,
+      maxLength: 15,
+      pattern: "^([0-9]{2}[0-9A-Z]{13})$",
+    },
+    directors: {
+      title: "Director",
+      type: "array",
+      items: {
+        // title: "Director",
+        type: "object",
+        properties: {
+          name: {
+            type: "string",
+          },
+          PANNumber: {
+            type: "string",
+            title: "PAN Number (ex: BNZAA2318J)",
+            minLength: 10,
+            maxLength: 10,
+            pattern: "^([A-Z]{5}[0-9]{4}[A-Z]{1})$",
+          },
+          tags: {
+            // description: "Tags",
+            title: "Tags",
+            type: "array",
+            items: {
+              type: "string",
+            },
+            uniqueItems: true,
+          },
+        },
+      },
     },
   },
+  required: ["name", "GSTIN", "directors"],
 };
 
 function App() {
